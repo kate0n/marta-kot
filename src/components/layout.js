51d538a1d44d4045afc561a623e3ecbcb8db8039
fmakareev <fmakareev@code-artel.com>
2019-12-02 14:43:39 +0300

    add ResponsiveImage

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,31 @@
 import React from "react"
 import Header from "./header/header"
-
+import { ResponsiveImage, ResponsiveImageSize } from 'react-responsive-image';
 const Layout = ({ children, bg, isMain }) => {
   return (
     <div
       className="app-wrapper"
-      style={{ backgroundImage: `url("${bg}")`, height: `${isMain && "100%"}` }}
     >
+      <ResponsiveImage>
+        <ResponsiveImageSize
+          default
+          minWidth={0}
+          path={'/images/homepage-bg-768.jpg'}
+        />
+        <ResponsiveImageSize
+          minWidth={1024}
+          path={'/images/homepage-bg-1024.jpg'}
+        />
+        <ResponsiveImageSize
+          minWidth={1440}
+          path={'/images/homepage-bg-1440.jpg'}
+        />
+        <ResponsiveImageSize
+          minWidth={1600}
+          path={'/images/homepage-bg-fullsize.jpg'}
+        />
+      </ResponsiveImage>
+
       <div className="outer-container">
         <Header />
         <main>{children}</main>
