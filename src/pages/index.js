580883b54210ba82f704b677478b127f077fcded
fmakareev <fmakareev@code-artel.com>
2019-12-04 12:22:18 +0300

    remove mock data.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,11 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Footer from "../components/footer/footer"
 import Banner from "../components/banner/banner"
 
-// временные
-import IconYoutube from "../images/Youtube.svg"
-import IconFacebook from "../images/facebook.svg"
-import IconInstagram from "../images/insta.svg"
-import IconVK from "../images/vk.svg"
-import IconOK from "../images/ok.svg"
-import { graphql } from "gatsby"
-
 export const isBrowser = () => typeof window !== "undefined"
-const homePage = {
-  background: {
-    xs: "/images/homepage-bg-768.jpg",
-    sm: "/images/homepage-bg-1024.jpg",
-    md: "/images/homepage-bg-1440.jpg",
-    lg: "/images/homepage-bg-fullsize.jpg",
-  },
-  socialList: [
-    { name: "YouTube", image: IconYoutube, url: "YouTube" },
-    { name: "Facebook", image: IconFacebook, url: "Facebook" },
-    { name: "Instagram", image: IconInstagram, url: "Instagram" },
-    { name: "VK", image: IconVK, url: "VK" },
-    { name: "OK", image: IconOK, url: "OK" },
-  ],
-  track: "https://www.milannohejl.cz/subdom/codepen/Shantifax-KukuPuja.mp3",
-  mainBanner: {
-    content:
-      "22.10.2019 — Москва, «Флакон», МосВегФест 2019 • 22.10.2019 — Москва, «Флакон», МосВегФест 2019 • 22.10.2019 Москва, «Флакон», МосВегФест 2019 • ",
-    visibly: true,
-  },
-}
-// временные
+
 
 const IndexPage = props => {
   const { getHomePage, getConcerts } = props.data.marta
@@ -45,7 +17,7 @@ const IndexPage = props => {
         <Footer {...getHomePage} />
       </Layout>
       {
-        homePage.mainBanner.visibly && (
+        getHomePage.mainBanner.visibly && (
           <Banner content={getHomePage.mainBanner.content} url="#"/>
         )
       }
