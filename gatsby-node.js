ebbffd152ac914c23a9480fdda41f0836a2246a7
fmakareev <fmakareev@code-artel.com>
2019-12-04 19:38:43 +0300

    add createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
 
-// You can delete this file if you're not using it
+const rebuildPages = typeof process.env.REBUILD_PAGES === "string" ? process.env.REBUILD_PAGES.split(",") : []
+
+
+const pagesPath ={
+  'video':'/video/',
+  'music':'/music/',
+  'contacts':'/contacts/',
+  'concert':'/concert/',
+  'index':'/',
+}
+
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+  if(process.env.NODE_ENV === 'production'){
+    if (rebuildPages.length) {
+      rebuildPages.forEach((item)=>{
+        createPage({
+          path: pagesPath[item],
+          component: path.resolve(`./src/templates/${item}.js`),
+        })
+      })
+      return
+    }
+  }
+
+  Object.entries(pagesPath).forEach(([key,value])=>{
+    createPage({
+      path: value,
+      component: path.resolve(`./src/templates/${key}.js`),
+    })
+  })
+
+}
