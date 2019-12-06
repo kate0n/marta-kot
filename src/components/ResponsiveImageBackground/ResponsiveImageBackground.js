080cd76d73a1ef43a892059a1cdf30c475f5663b
Ekaterina <mikhailina.ekaterina@gmail.com>
2019-12-06 15:54:34 +0300

    change mobile bg from xs to sm

--- src/components/ResponsiveImageBackground/ResponsiveImageBackground.js
+++ src/components/ResponsiveImageBackground/ResponsiveImageBackground.js
@@ -2,15 +2,32 @@ import React from "react"
 import { ResponsiveImage, ResponsiveImageSize } from "react-responsive-image"
 
 const ResponsiveImageBackground = ({ bg }) => {
-  if(!bg){
-    return null;
+  if (!bg) {
+    return null
   }
   return (
     <ResponsiveImage>
-      <ResponsiveImageSize alt={'background'} minWidth={0} path={bg.xs && bg.xs.url} />
-      <ResponsiveImageSize alt={'background'} minWidth={768} path={bg.sm && bg.sm.url} />
-      <ResponsiveImageSize alt={'background'} default minWidth={1024} path={bg.md && bg.md.url} />
-      <ResponsiveImageSize alt={'background'} minWidth={1440} path={bg.lg && bg.lg.url} />
+      <ResponsiveImageSize
+        alt={"background"}
+        minWidth={0}
+        path={bg.sm && bg.sm.url}
+      />
+      <ResponsiveImageSize
+        alt={"background"}
+        minWidth={768}
+        path={bg.sm && bg.sm.url}
+      />
+      <ResponsiveImageSize
+        alt={"background"}
+        default
+        minWidth={1024}
+        path={bg.md && bg.md.url}
+      />
+      <ResponsiveImageSize
+        alt={"background"}
+        minWidth={1440}
+        path={bg.lg && bg.lg.url}
+      />
     </ResponsiveImage>
   )
 }
